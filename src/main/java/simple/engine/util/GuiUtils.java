de467615c5e89543f0dda7ab864e76c784e9ebba
NastyGamer <jones.m.987654321@gmail.com>
2020-01-26 21:08:57 +0100

    Added some more GuiUtils

--- src/main/java/simple/engine/util/GuiUtils.java
+++ src/main/java/simple/engine/util/GuiUtils.java
@@ -7,7 +7,9 @@ import java.awt.*;
 import java.awt.font.FontRenderContext;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GuiUtils {
 
@@ -63,5 +65,52 @@ public class GuiUtils {
     public static <T extends Widget> T centerVertically(T widget) {
         widget.rectangle.y = Engine.getConfig().getHeight() / 2 - widget.rectangle.height / 2;
         return widget;
+    }
+
+    @SafeVarargs
+    public static <T extends Widget> T[] alignHorizontally(int padding, T... widgets) {
+        AtomicInteger currentX = new AtomicInteger(widgets[0].rectangle.x + widgets[0].rectangle.width + padding);
+        System.out.println(currentX.get());
+        final int y = widgets[0].rectangle.y;
+        for (int i = 1; i < widgets.length; i++) {
+            widgets[i].rectangle.x = currentX.get();
+            widgets[i].rectangle.y = y;
+            currentX.addAndGet(widgets[i].rectangle.width + padding);
+        }
+        return widgets;
+    }
+
+    @SafeVarargs
+    public static <T extends Widget> T[] alignVertically(int padding, T... widgets) {
+        AtomicInteger currentY = new AtomicInteger(widgets[0].rectangle.y + widgets[0].rectangle.width + padding);
+        final int x = widgets[0].rectangle.x;
+        for (int i = 1; i < widgets.length; i++) {
+            widgets[i].rectangle.y = currentY.get();
+            widgets[i].rectangle.x = x;
+            currentY.addAndGet(widgets[i].rectangle.height + padding);
+        }
+        return widgets;
+    }
+
+    public static Rectangle getBoundingBox(Widget... widgets) {
+        AtomicInteger x = new AtomicInteger(Integer.MAX_VALUE);
+        AtomicInteger y = new AtomicInteger(Integer.MAX_VALUE);
+        AtomicInteger w = new AtomicInteger(Integer.MIN_VALUE);
+        AtomicInteger h = new AtomicInteger(Integer.MIN_VALUE);
+        Arrays.stream(widgets).forEach(widget -> {
+            if (widget.rectangle.x < x.get()) x.set(widget.rectangle.x);
+            if (widget.rectangle.y < y.get()) y.set(widget.rectangle.y);
+            if (widget.rectangle.width > w.get()) w.set(widget.rectangle.width);
+            if (widget.rectangle.height > w.get()) w.set(widget.rectangle.height);
+        });
+        return new Rectangle(x.get(), y.get(), w.get(), h.get());
+    }
+
+    public static <T extends Widget> T[] translate(int x, int y, T[] widgets) {
+        Arrays.stream(widgets).forEach(widget -> {
+            widget.rectangle.x += x;
+            widget.rectangle.y += y;
+        });
+        return widgets;
     }
 }
