94d869b3040bf202e1d045c4c0fe2abacc866478
NastyGamer <jones.m.987654321@gmail.com>
2020-02-01 15:15:17 +0100

    Added the ability to store and retrieve volatile values on the fly in the StorageModule

--- src/main/java/simple/engine/modules/StorageModule.java
+++ src/main/java/simple/engine/modules/StorageModule.java
@@ -1,29 +1,27 @@
 package simple.engine.modules;
 
 import org.apache.commons.io.FilenameUtils;
-import simple.engine.util.GameConfig;
 import simple.engine.graphics.Animation;
 import simple.engine.loaders.GifLoader;
 import simple.engine.loaders.ImageLoader;
 import simple.engine.loaders.SoundLoader;
 import simple.engine.util.ArrayUtils;
+import simple.engine.util.GameConfig;
 
 import javax.sound.sampled.Clip;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
 
 public class StorageModule extends Module {
 
+    private final HashMap<Integer, Object> values = new HashMap<>();
     private final ImageLoader imageLoader = new ImageLoader();
     private final SoundLoader soundLoader = new SoundLoader();
     private final GifLoader gifLoader = new GifLoader();
     private final Class<?> clazz;
-
-    public File getRelativeFile(String path) {
-        return new File(clazz.getProtectionDomain().getCodeSource().getLocation().getPath().concat(path));
-    }
 
     public StorageModule(GameConfig config, Class<?> clazz) {
         super(config);
@@ -41,6 +39,20 @@ public class StorageModule extends Module {
         } catch (IOException e) {
             System.err.println(e.getCause().getMessage());
         }
+    }
+
+    public File getRelativeFile(String path) {
+        return new File(clazz.getProtectionDomain().getCodeSource().getLocation().getPath().concat(path));
+    }
+
+    public void putValue(int id, Object o) {
+        values.put(id, o);
+    }
+
+    public <T> T retrieveValue(int id, Class<T> c) {
+        if (!values.containsKey(id))
+            throw new IllegalArgumentException("Widget with ID ".concat(String.valueOf(id)).concat(" doesn't exist"));
+        return c.cast(values.get(id));
     }
 
     public Animation getAnimation(String name) {
